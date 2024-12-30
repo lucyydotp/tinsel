6cde394d342ecb395c5bb838bc8b58aec5879437
Lucy Poulton <lucy@lucyydotp.me>
2024-12-30 13:40:43 +0000

    Add . to camel case split chars

--- gradle-plugin/src/main/kotlin/me/lucyydotp/tinsel/gradle/util/StringUtil.kt
+++ gradle-plugin/src/main/kotlin/me/lucyydotp/tinsel/gradle/util/StringUtil.kt
@@ -4,5 +4,5 @@ package me.lucyydotp.tinsel.gradle.util
  * Camel-cases a string, replacing underscores with capital letters.
  */
 public fun String.camelCase(): String = this
-    .split("_", "-")
+    .split('_', '-', '.')
     .joinToString("") { it.replaceFirstChar(Char::uppercase) }
